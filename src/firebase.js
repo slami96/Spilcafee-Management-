c5790e48aa6c1e7bb968875d95658341ea040b3b
slami96 <adam.slamen@gmail.com>
2025-03-11 22:20:49 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqQcQFLQjDxLdQKnB3hCUj-3NIwWXB4Mk",
-  authDomain: "spilcafe-games-management.firebaseapp.com",
-  projectId: "spilcafe-games-management",
-  storageBucket: "spilcafe-games-management.appspot.com",
-  messagingSenderId: "1073953765667",
-  appId: "1:1073953765667:web:22b2de3eaaa476bb527496"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
