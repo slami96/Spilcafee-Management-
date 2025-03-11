a170d23b7c7d317aaf43f2e087bee6ab2f28814d
slami96 <adam.slamen@gmail.com>
2025-03-11 17:26:50 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyAqQcQFLQjDxLdQKnB3hCUj-3NIwWXB4Mk",
+  authDomain: "spilcafe-games-management.firebaseapp.com",
+  projectId: "spilcafe-games-management",
+  storageBucket: "spilcafe-games-management.firebasestorage.app",
+  messagingSenderId: "1073953765667",
+  appId: "1:1073953765667:web:22b2de3eaaa476bb527496"
 };
 
 // Initialize Firebase
