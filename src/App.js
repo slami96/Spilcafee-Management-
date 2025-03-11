03bdcf988d1b14ba754958c27bc82ce58d5e69fa
slami96 <adam.slamen@gmail.com>
2025-03-11 17:53:48 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// src/App.js
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import './styles.css';
+import Dashboard from './components/Dashboard'; // This will be your game management screen
 
 function App() {
   return (
@@ -12,16 +11,9 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route 
-            path="/dashboard" 
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            } 
-          />
-          {/* Add more protected routes for other components */}
-          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </Router>
