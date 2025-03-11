2b4e3e1ce1ad27506881ffa68a0cd7a333283169
slami96 <adam.slamen@gmail.com>
2025-03-11 19:58:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// Remove this line completely
+// import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
