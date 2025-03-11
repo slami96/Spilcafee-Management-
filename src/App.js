9ecdcdded81ab3c6a7bc11a2cbb4f80e085098a2
slami96 <adam.slamen@gmail.com>
2025-03-11 22:24:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAuth } from './contexts/AuthContext';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login';
