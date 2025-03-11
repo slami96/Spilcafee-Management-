cecad57411ff2c7e6788b438e78f012f34fb4e2a
slami96 <adam.slamen@gmail.com>
2025-03-11 17:23:01 +0100

    Create PrivateRoute.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+// src/components/PrivateRoute.jsx
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function PrivateRoute({ children }) {
+  const { currentUser, loading } = useAuth();
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  
+  return currentUser ? children : <Navigate to="/login" />;
+}
